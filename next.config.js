f736ca0c1bcd9c40a25c795793ee7ba476458c03
jkrbx5v4sq-png <jkrbx5v4sq@privaterelay.appleid.com>
2026-02-20 14:56:09 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://www.mshdigital.de https://mshdigital.de;"
+          }
+        ]
+      }
+    ];
+  }
+};
 
 export default nextConfig;
